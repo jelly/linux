da8215e68981761085cbf860d363121d4dce8929
Jelle van der Waa <jvanderwaa@redhat.com>
2025-06-02 13:40:00 +0200

    platform/x86: fujitsu: use unsigned int for kstrtounit
    Signed-off-by: Jelle van der Waa <jvanderwaa@redhat.com>

--- drivers/platform/x86/fujitsu-laptop.c
+++ drivers/platform/x86/fujitsu-laptop.c
@@ -176,7 +176,8 @@ static ssize_t charge_control_end_threshold_store(struct device *dev,
 				const char *buf, size_t count)
 {
 	int cc_end_value, s006_cc_return;
-	int value, ret;
+	int ret;
+	unsigned int value;
 
 	ret = kstrtouint(buf, 10, &value);
 	if (ret)
